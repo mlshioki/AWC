76bbe45d8c3fb9927b9c79d3edb5bb69f3e04592
Sayuri Hioki <sayuri.hioki12@gmail.com>
2021-03-12 19:17:05 -0300

    aula 26/02

---
+++ senac-tsi-mvc/app/Models/Funcionario.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Funcionario extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'id',
+                            'nome',
+                            'endereco',
+                            'email',
+                            'telefone'];
+
+    protected $table = 'Funcionario';
+
+    /* Como mudar a chave primária:
+    protected $primaryKey = 'nome-desejado';
+
+    Deixar de ser auto increment:
+    public $increment = false;
+
+    Para definir tipo:
+    protected $keyType = 'string';
+
+    Para retirar campos timestamps:
+    public $timestamps = false;
+    */
+
+}
